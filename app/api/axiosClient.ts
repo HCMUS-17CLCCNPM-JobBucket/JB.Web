913f57d7ec6588ce7bc633c269b430a831fd541e
DerekZohar <62027015+DerekZohar@users.noreply.github.com>
2021-10-23 19:34:55 +0700

    Update axiosClient.ts

--- app/api/axiosClient.ts
+++ app/api/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 // Set up default config for http requests here
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://jobbucket.azurewebsites.net",
   headers: {
     "content-type": "application/json",
   },
