524ded10f445ebadefd047d7b60878f8005b768b
DerekZohar <62027015+DerekZohar@users.noreply.github.com>
2021-10-29 14:05:10 +0700

    Update company card

--- app/components/atoms/CompanyCard.tsx
+++ app/components/atoms/CompanyCard.tsx
@@ -22,7 +22,7 @@ export default function CompanyCard(props) {
         src={props.avatarUrl}
         alt={props.name}
       />
-      <div className="px-4 py-2">
+      <div className="px-4 py-2 flex-1">
         <div className="flex justify-between">
           <div>
             <p
@@ -37,32 +37,74 @@ export default function CompanyCard(props) {
             </span>
           </div>
           <div className="flex items-center">
-            <button
-              type="button"
-              className="w-full flex items-center border-l border-t border-b text-base 
-              font-medium rounded-l-md text-black bg-white hover:bg-gray-100 p-2"
-            >
+            <button type="button" className="flex gap-2 items-center">
+              4.5
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width={20}
                 height={20}
-                className="w-4 h-4 mr-2"
+                className="w-4 h-4 mr-2 text-yellow-400"
                 fill="currentColor"
                 viewBox="0 0 1792 1792"
               >
                 <path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
               </svg>
-              Star
-            </button>
-            <button
-              type="button"
-              className="w-full border text-base font-medium rounded-r-md text-black bg-white hover:bg-gray-100 px-4 py-2"
-            >
-              654
             </button>
           </div>
         </div>
-        <div className="mt-4 line-clamp-3">{props.bio}</div>
+        {/* <div className="mt-4 line-clamp-3">{props.bio}</div> */}
+        <div className="mt-4">
+          {/* <div className="flex gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 text-gray-400"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <p>{props.address}</p>
+          </div> */}
+          <div className="flex gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 text-gray-400"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+            </svg>
+            <p>{props.phoneNumber}</p>
+          </div>
+          <div className="flex gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 text-gray-400"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+              <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+            </svg>
+            <p>{props.email}</p>
+          </div>
+          <div className="flex gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 text-gray-400"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path d="m12 17 1-2V9.858c1.721-.447 3-2 3-3.858 0-2.206-1.794-4-4-4S8 3.794 8 6c0 1.858 1.279 3.411 3 3.858V15l1 2z"></path>
+              <path d="m16.267 10.563-.533 1.928C18.325 13.207 20 14.584 20 16c0 1.892-3.285 4-8 4s-8-2.108-8-4c0-1.416 1.675-2.793 4.267-3.51l-.533-1.928C4.197 11.54 2 13.623 2 16c0 3.364 4.393 6 10 6s10-2.636 10-6c0-2.377-2.197-4.46-5.733-5.437z"></path>
+            </svg>
+            <p>{props.country}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ pages/job/post.tsx
@@ -0,0 +1,165 @@
+import { Tab } from "@headlessui/react";
+import { blogAPI } from "app/api/modules/blogAPI";
+import { imageAPI } from "app/api/modules/imageAPI";
+import BlogTagSelection from "app/components/molecules/BlogTagSelection";
+import helper from "app/utils/helper";
+import { useFormik } from "formik";
+import dynamic from "next/dynamic";
+import router from "next/router";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+
+const config = {
+  placeholderText: "Edit Your Content Here!",
+  charCounterCount: true,
+  imageUploadURL: "https://api.cloudinary.com/v1_1/derekzohar/image/upload",
+  imageUploadParams: {
+    api_key: "866395791528912",
+    upload_preset: "images",
+  },
+  imageUploadMethod: "POST",
+  events: {
+    "froalaEditor.image.uploaded": (e, editor, response) => {
+      response = JSON.parse(response);
+      editor.image.insert(response.url, true, null, editor.image.get(), null);
+      console.log(response);
+      // return false;
+    },
+  },
+};
+const FroalaEditorComponent: React.ComponentType<any> = dynamic(
+  () => {
+    return new Promise((resolve) =>
+      import("froala-editor/js/plugins.pkgd.min.js").then((e) => {
+        import("react-froala-wysiwyg").then(resolve);
+      })
+    );
+  },
+  {
+    loading: () => null,
+    ssr: false,
+  }
+);
+export default function AddNewJob() {
+  const user = useSelector((state: any) => state.user);
+  const [isUploadImg, setIsUploadImg] = useState(true);
+  const [content, setContent] = useState("");
+  const [imageFile, setImageFile] = useState(null);
+  const [previewSource, setPreviewSource] = useState("");
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImageFile(file);
+    setPreviewSource(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      imageUrls: [],
+      description: "",
+      activeStatus: 0,
+      priority: 0,
+      addresses: [""],
+      cities: [""],
+      minSalary: 0,
+      maxSalary: 0,
+      salaryCurrency: "",
+      salaryDuration: "",
+      skills: [],
+      positions: [],
+      applications: [],
+      applicationCount: 0,
+      interests: [],
+      interestCount: 0,
+      types: [],
+      categories: [],
+      isVisaSponsorship: false,
+      employerId: 0,
+      employer: null,
+      expireDate: null,
+      benefits: "",
+      experiences: "",
+      responsibilities: "",
+      requirements: "",
+      optionalRequirements: "",
+      cultures: "",
+      whyJoinUs: "",
+      numberEmployeesToApplied: 0,
+      jobForm: "",
+      gender: 0,
+      views: 0,
+      isJobInterested: false,
+      isJobApplied: false,
+      organizationId: 0,
+      organization: null,
+    },
+    onSubmit: async (values) => {
+      const imageRes: any = await imageAPI.uploadImage(imageFile);
+      const res = await blogAPI.add(
+        {
+          ...values,
+          imageUrl: imageRes.data.url,
+          content,
+        },
+        user.token
+      );
+
+      if (res.status === 200) router.push("/blog/" + res.data.data.blog.add.id);
+    },
+  });
+
+  return (
+    <form
+      className="px-48 py-4 flex flex-col gap-4"
+      onSubmit={formik.handleSubmit}
+    >
+      <img
+        src={previewSource || "https://via.placeholder.com/1134x160"}
+        alt=""
+        className="h-40 w-full object-cover rounded-lg"
+      />
+      <input type="file" onChange={handleImageChange} />
+      {/* <div className="flex justify-between">
+        <div></div>
+      </div> */}
+
+      <BlogTagSelection />
+      <input
+        type="text"
+        id="title"
+        name="title"
+        value={formik.values.title}
+        onChange={formik.handleChange}
+        className=" rounded-lg border-transparent flex-1 appearance-none 
+          border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 
+          placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 
+          focus:ring-purple-600 focus:border-transparent"
+        placeholder="Title"
+      />
+      <label className="text-gray-700">
+        <textarea
+          className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 
+          bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base 
+          focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+          id="description"
+          placeholder="Enter your description"
+          name="description"
+          value={formik.values.description}
+          onChange={formik.handleChange}
+          rows={5}
+          cols={40}
+        ></textarea>
+      </label>
+      <FroalaEditorComponent
+        tag="textarea"
+        config={config}
+        model={content}
+        onModelChange={(model) => setContent(model)}
+      />
+
+      <button className="btn btn-primary w-40" type="submit">
+        Post
+      </button>
+    </form>
+  );
+}
