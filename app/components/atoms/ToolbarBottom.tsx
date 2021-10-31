1b1f10ca6655b25da364f6e246821272380afe97
DerekZohar <62027015+DerekZohar@users.noreply.github.com>
2021-10-31 15:39:37 +0700

    Update handle scroll to top
    show scroll to top button if scroll position is greater than 100

---
+++ app/components/atoms/ToolbarBottom.tsx
@@ -0,0 +1,50 @@
+import helper from "app/utils/helper";
+import React, { useEffect, useState } from "react";
+
+export default function ToolbarBottom() {
+  const [isShow, setIsShow] = useState(false);
+  const handleScrollToTop = () => helper.scrollToTop();
+
+  useEffect(() => {
+    //show scroll to top button if scroll position is greater than 100
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsShow(true);
+      } else {
+        setIsShow(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div className="fixed bottom-6 right-6 flex flex-col gap-2 w-10">
+      <button
+        onClick={handleScrollToTop}
+        className={
+          (!isShow && "hidden") + " p-2 rounded-full border border-gray-700"
+        }
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M5 15l7-7 7 7"
+          />
+        </svg>
+      </button>
+      <button onClick={handleScrollToTop} className="">
+        <img src="common/chat.png" alt="chat" className="h-10 w-10" />
+      </button>
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
 import LoadingTransition from "app/components/atoms/LoadingTransition";
 import Alerts from "app/components/atoms/notification/alerts";
+import ToolbarBottom from "app/components/atoms/ToolbarBottom";
 import AuthProvider from "app/components/layouts/AuthProvider";
 import { GoogleAuthProvider } from "app/components/layouts/google-provider";
 import Footer from "app/components/organisms/Footer";
 import Navbar from "app/components/organisms/Navbar";
 import { persistor, store } from "app/redux/store";
-import helper from "app/utils/helper";
 import "froala-editor/css/froala_editor.pkgd.min.css";
 import "froala-editor/css/froala_style.min.css";
 import type { AppProps } from "next/app";
 import Router from "next/router";
-import { useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import "../styles/globals.scss";
@@ -18,7 +18,6 @@ import "../styles/globals.scss";
 function MyApp({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(false);
 
-  const handleScrollToTop = () => helper.scrollToTop();
   Router.events.on("routeChangeStart", (url) => {
     // if (router.pathname.includes("/chat") !== true) {
     setLoading(true);
@@ -38,31 +37,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
             <Navbar />
             <Component {...pageProps} />
-            <div className="fixed bottom-6 right-6 flex flex-col gap-2">
-              <button
-                onClick={handleScrollToTop}
-                className="p-2 rounded-full border border-gray-700"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M5 15l7-7 7 7"
-                  />
-                </svg>
-              </button>
-              <button onClick={handleScrollToTop} className="">
-                <img src="common/chat.png" alt="chat" className="h-10 w-10" />
-              </button>
-            </div>
-
+            <ToolbarBottom />
             <Footer />
           </AuthProvider>
         </PersistGate>
