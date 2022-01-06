a8f11e780d8168c9bd7fe1a58b978cc3cb0d7b06
DerekZohar <62027015+DerekZohar@users.noreply.github.com>
2022-01-06 17:08:54 +0700

    fix bug 2

--- app/components/atoms/Button/InterviewButton.tsx
+++ app/components/atoms/Button/InterviewButton.tsx
@@ -64,7 +64,7 @@ export default function InterviewButton(props) {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Dialog.Overlay className="fixed inset-0 bg-white opacity-75" />
+              <Dialog.Overlay className="fixed inset-0  bg-white opacity-75" />
             </Transition.Child>
 
             {/* This element is to trick the browser into centering the modal contents. */}
@@ -83,7 +83,7 @@ export default function InterviewButton(props) {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="inline-block w-full max-w-[600px] min-h-[200px]  p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-my rounded-2xl">
+              <div className="inline-block w-full  max-w-[600px] min-h-[200px] p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
                 <Dialog.Title
                   as="h3"
                   className="text-lg leading-6 text-gray-900 font-semibold"
--- app/components/atoms/Button/SetScheduleInterviewButton.tsx
+++ app/components/atoms/Button/SetScheduleInterviewButton.tsx
@@ -46,7 +46,7 @@ export default function SetScheduleInterviewButton({
   });
 
   return (
-    <>
+    <div>
       <div className="">
         <button onClick={openModal} className="btn btn-primary w-40">
           Interview
@@ -88,14 +88,14 @@ export default function SetScheduleInterviewButton({
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="inline-block w-full max-w-[600px] min-h-[400px]  p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-my rounded-2xl">
+              <div className="inline-block w-full  max-w-[600px] min-h-[200px] p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
                 <Dialog.Title
                   as="h3"
                   className="text-lg leading-6 text-gray-900 font-semibold"
                 >
                   Interview Form
                 </Dialog.Title>
-                <form
+                {/* <form
                   onSubmit={formik.handleSubmit}
                   className="flex flex-col gap-4 mt-4"
                 >
@@ -126,12 +126,12 @@ export default function SetScheduleInterviewButton({
                       Add
                     </button>
                   </div>
-                </form>
+                </form> */}
               </div>
             </Transition.Child>
           </div>
         </Dialog>
       </Transition>
-    </>
+    </div>
   );
 }
